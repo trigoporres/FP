1f19de591e50bf67bad2c4cce5617fb5778d9ea8
trigoporres <santiagotrigoporres@gmail.com>
2020-06-30 00:00:15 +0200

    add examen febrero 2019

--- src/components/generateExamn.js
+++ src/components/generateExamn.js
@@ -4,11 +4,10 @@ import Questions from "./questions"
 
 function getProjectsList(data) {
   let value = []
-  debugger
-  data.allPruebaJson.edges.forEach(item => {
+  data.allFebrero2019AJson.edges.forEach(item => {
+    debugger
     value.push(<Questions item={item.node} />)
   })
-  debugger
   return value
 }
 
@@ -16,14 +15,96 @@ const GenerateExamen = () => (
   <StaticQuery
     query={graphql`
       {
-        allPruebaJson {
+        allFebrero2019AJson {
           edges {
             node {
               Feb {
                 _1 {
                   a
+                  c
+                  b
+                  code
+                  d
+                  question
+                  reply
+                }
+                _10 {
+                  a
                   b
                   c
+                  code
+                  d
+                  question
+                  reply
+                }
+                _2 {
+                  a
+                  b
+                  c
+                  code
+                  d
+                  question
+                  reply
+                }
+                _3 {
+                  a
+                  b
+                  c
+                  code
+                  d
+                  question
+                  reply
+                }
+                _4 {
+                  a
+                  b
+                  c
+                  code
+                  d
+                  question
+                  reply
+                }
+                _5 {
+                  b
+                  a
+                  code
+                  c
+                  d
+                  question
+                  reply
+                }
+                _6 {
+                  a
+                  b
+                  code
+                  c
+                  d
+                  question
+                  reply
+                }
+                _7 {
+                  a
+                  b
+                  c
+                  code
+                  d
+                  question
+                  reply
+                }
+                _8 {
+                  a
+                  b
+                  code
+                  c
+                  question
+                  d
+                  reply
+                }
+                _9 {
+                  a
+                  b
+                  c
+                  code
                   d
                   question
                   reply
--- src/components/questions.js
+++ src/components/questions.js
@@ -5,6 +5,52 @@ const Questions = item => (
     <p>{item.item.Feb._1.question}</p>
     <input type="radio" value="a" />
     <label for="a">{item.item.Feb._1.a}</label>
+    <input type="radio" value="b" />
+    <label for="b">{item.item.Feb._1.b}</label>
+    <input type="radio" value="c" />
+    <label for="c">{item.item.Feb._1.c}</label>
+    <input type="radio" value="d" />
+    <label for="d">{item.item.Feb._1.d}</label>
+    <br></br>
+    <p>{item.item.Feb._2.question}</p>
+    <input type="radio" value="a" />
+    <label for="a">{item.item.Feb._2.a}</label>
+    <input type="radio" value="b" />
+    <label for="b">{item.item.Feb._2.b}</label>
+    <input type="radio" value="c" />
+    <label for="c">{item.item.Feb._2.c}</label>
+    <input type="radio" value="d" />
+    <label for="d">{item.item.Feb._2.d}</label>
+    <br></br>
+    <p>{item.item.Feb._3.question}</p>
+    <input type="radio" value="a" />
+    <label for="a">{item.item.Feb._3.a}</label>
+    <input type="radio" value="b" />
+    <label for="b">{item.item.Feb._3.b}</label>
+    <input type="radio" value="c" />
+    <label for="c">{item.item.Feb._3.c}</label>
+    <input type="radio" value="d" />
+    <label for="d">{item.item.Feb._3.d}</label>
+    <br></br>
+    <p>{item.item.Feb._4.question}</p>
+    <input type="radio" value="a" />
+    <label for="a">{item.item.Feb._4.a}</label>
+    <input type="radio" value="b" />
+    <label for="b">{item.item.Feb._4.b}</label>
+    <input type="radio" value="c" />
+    <label for="c">{item.item.Feb._4.c}</label>
+    <input type="radio" value="d" />
+    <label for="d">{item.item.Feb._4.d}</label>
+    <br></br>
+    <p>{item.item.Feb._5.question}</p>
+    <input type="radio" value="a" />
+    <label for="a">{item.item.Feb._5.a}</label>
+    <input type="radio" value="b" />
+    <label for="b">{item.item.Feb._5.b}</label>
+    <input type="radio" value="c" />
+    <label for="c">{item.item.Feb._5.c}</label>
+    <input type="radio" value="d" />
+    <label for="d">{item.item.Feb._5.d}</label>
     <br></br>
   </React.Fragment>
 )
